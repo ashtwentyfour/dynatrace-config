316aa2450dcc4e436da8ac17cb319eeb3b5861a2
Ashwin Menon <ashtwentyfour@ashwins-air.home>
2024-11-03 22:22:21 -0500

    updated mzone rules

--- mzone.tf
+++ mzone.tf
@@ -1,4 +1,86 @@
-resource "dynatrace_management_zone_v2" "Azure-Dev" {
-    name = "AzureDev"
-    description = "Azure Dev/Sandbox resources"
+resource "dynatrace_management_zone_v2" "mzone_azure_aks_dev" {
+    name = "AzureAKSDev"
+    description = "Azure Dev/Sandbox Azure Kubernetes Service (AKS) resources"
+    rules {
+        rule {
+            type            = "ME"
+            enabled         = true
+            entity_selector = ""
+            attribute_rule {
+                entity_type = "CLOUD_APPLICATION"
+                attribute_conditions {
+                    condition {
+                        case_sensitive = false
+                        key            = "KUBERNETES_CLUSTER_NAME"
+                        operator       = "BEGINS_WITH"
+                        string_value   = "aks-dev"
+                    }
+                }
+            }
+        }
+        rule {
+            type            = "ME"
+            enabled         = true
+            entity_selector = ""
+            attribute_rule {
+                entity_type = "KUBERNETES_CLUSTER"
+                attribute_conditions {
+                    condition {
+                        case_sensitive = false
+                        key            = "KUBERNETES_CLUSTER_NAME"
+                        operator       = "BEGINS_WITH"
+                        string_value   = "aks-dev"
+                    }
+                }
+            }
+        }
+        rule {
+            type            = "ME"
+            enabled         = true
+            entity_selector = ""
+            attribute_rule {
+                entity_type = "CLOUD_APPLICATION_NAMESPACE"
+                attribute_conditions {
+                    condition {
+                        case_sensitive = false
+                        key            = "KUBERNETES_CLUSTER_NAME"
+                        operator       = "BEGINS_WITH"
+                        string_value   = "aks-dev"
+                    }
+                }
+            }
+        }
+        rule {
+            type            = "ME"
+            enabled         = true
+            entity_selector = ""
+            attribute_rule {
+                entity_type = "KUBERNETES_SERVICE"
+                attribute_conditions {
+                    condition {
+                        case_sensitive = false
+                        key            = "KUBERNETES_CLUSTER_NAME"
+                        operator       = "BEGINS_WITH"
+                        string_value   = "aks-dev"
+                    }
+                }
+            }
+        }
+        rule {
+            type            = "ME"
+            enabled         = true
+            entity_selector = ""
+            attribute_rule {
+                entity_type = "HOST"
+                attribute_conditions {
+                    condition {
+                        case_sensitive = false
+                        key            = "HOST_NAME"
+                        operator       = "BEGINS_WITH"
+                        string_value   = "aks"
+                    }
+                }
+            }
+        }
+    }
 }
