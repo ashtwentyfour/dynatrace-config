ad9b7af55fa55e6736c0fdc466c8f65d5f529f61
Ashwin Menon <ashtwentyfour@ashwins-air.home>
2024-11-09 12:15:01 -0500

    added notification settings

---
+++ notification.tf
@@ -0,0 +1,11 @@
+resource "dynatrace_email_notification" "notification_azure_aks_dev" {
+  active                 = true
+  name                   = "AzureAKSDev-Notifications" 
+  profile                = dynatrace_alerting.alerting_azure_aks_dev.id
+  subject                = "{State} Problem {ProblemID}: {ImpactedEntity}"
+  to                     = [var.azure_default_notification_email]
+  cc                     = [var.azure_default_notification_cc_email]
+  bcc                    = [var.azure_default_notification_bcc_email]
+  notify_closed_problems = true
+  body                   = "{ProblemDetailsHTML}"
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable "azure_aks_dev_vmss" {
     type = string
     default = "aks-agentpool-38499208-vmss"
 }
+
+variable "azure_default_notification_email" {
+    type = string
+    default = "azure_sre@sre.org"
+}
+
+variable "azure_default_notification_cc_email" {
+    type = string
+    default = "azure_sre@sre.org"
+}
+
+variable "azure_default_notification_bcc_email" {
+    type = string
+    default = "azure_sre@sre.org"
+}
